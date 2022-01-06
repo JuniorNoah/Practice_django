baa36695168b27149ab1678ce28a176891a25244
Hyeokgi <chinup1004@naver.com>
2022-01-06 11:14:34 +0900

    Hyeokgi

---
+++ Hyeokgi/practice/models.py
@@ -0,0 +1,45 @@
+from enum import unique
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
+
+from basic_practice.settings import AUTH_PASSWORD_VALIDATORS
+
+class User(models.Model):
+    SEX = [
+        ('M', "Man"),
+        ("G", "Girl"),
+    ]
+    
+    user_id = models.CharField(max_length=30, primary_key=True, unique=True)
+    user_pw = models.CharField(max_length=30)
+    user_email = models.CharField(max_length=128, unique=True)
+    user_name = models.CharField(max_length=30)
+    user_nick_name = models.CharField(max_length=30)
+    user_phone_num = models.CharField(max_length=30, unique=True)
+    sex = models.CharField(max_length=2, choices=SEX)
+    
+class Community(models.Model):
+    author_id = models.ForeignKey('User', on_delete=models.CASCADE, db_column='author_id')
+    
+class Blocked_User(models.Model):
+    community_id = models.ForeignKey('Community', on_delete=models.CASCADE, db_column='community_id')
+    blocked_user_id = models.ForeignKey('User', on_delete=models.CASCADE, db_column='user_id')
+    
+class Subject(models.Model):
+    community_id = models.ForeignKey('Community', on_delete=models.CASCADE, db_column='community_id')
+    subject = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    view_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateField(default=timezone.now)
+    writer = models.ForeignKey('User', on_delete=models.CASCADE, db_column='writer')
+    community_id = models.ForeignKey('Community', on_delete=models.CASCADE, db_column='community_id')
+        
+class Comment(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE, db_column='user_id')
+    post_id = models.ForeignKey('Post', on_delete=CASCADE, db_column='post_id')
+    text = models.TextField()
+    created_at = models.DateField(default=timezone.now)
---
+++ Hyeokgi/practice/serializers.py
@@ -0,0 +1,34 @@
+from django.db.models import fields
+from rest_framework import serializers
+from rest_framework.utils import field_mapping
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields = '__all__'
+
+class Blocked_UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blocked_User
+        fields = '__all__'
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+        
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
