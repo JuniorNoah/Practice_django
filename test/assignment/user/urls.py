14a44d475ed7eb28ddcc3642f21b5f37ce7c74cd
dsloud <hope3498@naver.com>
2022-01-10 10:24:59 +0900

    Jan

--- test/assignment/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserListView, UserDetailView, Loginview
-
-urlpatterns = [
-    path('crud/', UserListView.as_view()),
-    path('crud/<int:user_id>/', UserDetailView.as_view()),
-    path('login/', Loginview.as_view(), name='login'),
-]
