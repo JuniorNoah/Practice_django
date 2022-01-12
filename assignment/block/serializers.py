4d79e15d7f7807bd145f41a3bd739a5b82c95005
Hyeokgi <chinup1004@naver.com>
2022-01-12 16:00:43 +0900

    update

--- assignment/block/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Blacklist
-
-class BlockSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Blacklist
-        fields =  '__all__'
--- assignment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('community/', include('community.urls')),
-    path('block/', include('block.urls')),
-]
